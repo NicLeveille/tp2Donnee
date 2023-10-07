b681a5454d9fa79a41c2d2e3c382ad933af08ad1
NicLeveille <nicleveille@hotmail.ca>
2023-10-07 13:03:02 -0400

    eeee

--- API-Server-beta-1.85-main/controllers/MathsController.js
+++ API-Server-beta-1.85-main/controllers/MathsController.js
@@ -25,10 +25,14 @@ export default class MathsController extends Controller {
     }
     doOperation() {
 
+        if(parseFloat(this.HttpContext.path.params["x"]) == null){
+            console.log("rwewerwer");
+        }
         let op = this.HttpContext.path.params["op"];
-        let x = parseInt(this.HttpContext.path.params["x"]);
-        let y = parseInt(this.HttpContext.path.params["y"]);
-        let n = parseInt(this.HttpContext.path.params["n"]);
+        let x = parseFloat(this.HttpContext.path.params["x"]);
+        let y = parseFloat(this.HttpContext.path.params["y"]);
+        let n = parseFloat(this.HttpContext.path.params["n"]);
+        
 
 
         if (op == " ") {
@@ -52,16 +56,85 @@ export default class MathsController extends Controller {
             }
         }
         if (op == "%") {
-            if(y == 0){
+            if (y == 0) {
                 this.HttpContext.response.JSON({ op: op, x: x, y: y, value: "NaN" })
             }
-            else{
+            else {
                 this.HttpContext.response.JSON({ op: op, x: x, y: y, value: (x % y) })
             }
-           
+
+        }
+        if (op == "!") {
+            
+            if(n == 0){
+                this.HttpContext.response.JSON({ op: op, n: n, error: "Impossible with 0" })
+            }
+            else if(n < 0){
+                this.HttpContext.response.JSON({ op: op, n: n, error: "Numbers needs to be above 0" })
+            }else if (!Number.isInteger(n)){
+                this.HttpContext.response.JSON({ op: op, n: n, error:"n needs to be an Integer" })
+            }
+            else{
+                this.HttpContext.response.JSON({ op: op, n: n, value: (factorial(n)) })
+            }
+            
+        }
+        if (op == "p") {
+            if (n == 0) {
+                this.HttpContext.response.JSON({ op: op, n: n, error: "Impossible with 0" })
+            }
+            else if(!Number.isInteger(n)){
+                this.HttpContext.response.JSON({ op: op, n: n, error:"n needs to be an Integer" })
+            }
+            else{
+                this.HttpContext.response.JSON({ op: op, n: n, value: (isPrime(n)) })
+            }
+
+        }
+        if (op == "np") {
+
+
+            if (n == 0) {
+                this.HttpContext.response.JSON({ op: op, n: n, error: "Impossible with 0" })
+            }
+            else if(!Number.isInteger(n)){
+                this.HttpContext.response.JSON({ op: op, n: n, error:"n needs to be an Integer" })
+            }
+            else{
+                this.HttpContext.response.JSON({ op: op, n: n, value: (findPrime(n)) })
+            }
+
+
         }
 
 
     }
 
+
+}
+
+function findPrime(n) {
+    let primeNumer = 0;
+    for (let i = 0; i < n; i++) {
+        primeNumer++;
+        while (!isPrime(primeNumer)) {
+            primeNumer++;
+        }
+    }
+    return primeNumer;
+}
+function factorial(n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+function isPrime(value) {
+    for (var i = 2; i < value; i++) {
+        if (value % i === 0) {
+            return false;
+        }
+    }
+    return value > 1;
 }
